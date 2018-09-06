3694cc591450cd098bc1c955b1a30049db666b6a
at7211 <at7211@gmail.com>
2018-09-06 12:23:57 +0800

    test

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -2,8 +2,7 @@
 
 function capitalize(str) {
   var char = str[0];
-  var strEn = new RegExp("[A-Za-z+]");
-  if(char === strEn){
+  if(char >= 'a' && char <= 'z'){
 	str = str.replace(char, function(s){return s.toUpperCase();});
   }
 	return str;
@@ -11,5 +10,4 @@ function capitalize(str) {
 }
 
 
-
-
+console.log("timing");
