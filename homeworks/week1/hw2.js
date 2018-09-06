0b35c618e744d8061adb13eef159b114d9543e79
at7211 <at7211@gmail.com>
2018-09-06 12:28:49 +0800

    fixed

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,13 +1,13 @@
 // #2 //
 
 function capitalize(str) {
-  var char = str[0];
-  if(char >= 'a' && char <= 'z'){
-	str = str.replace(char, function(s){return s.toUpperCase();});
-  }
-	return str;
-	
+    var char = str[0];
+    if (char >= 'a' && char <= 'z') {
+        str = str.replace(char, function(s) { return s.toUpperCase(); });
+    }
+    return str;
+
 }
 
 
-console.log("timing");
+console.log(capitalize("timing"));
