6aa67d95b54549fc615ef79264db700b6ebc37ea
at7211 <at7211@gmail.com>
2018-09-08 14:40:15 +0800

    hw2-1 change cortect

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,15 @@
 function stars(n) {
+	var image = '';
+	var Newimage= [];
 
+	if(1 <= n <=30){
+
+		for(var i=1; i<=n; i++){
+			image += '*';
+			Newimage[i-1] = image;
+		}
+	}
+	return Newimage;
 }
 
-module.exports = stars;
+module.exports = stars;    
