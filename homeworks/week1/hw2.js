2bdf4928f542bf2c7ceaacf3055f72ae6376d907
at7211 <at7211@gmail.com>
2018-09-08 13:34:00 +0800

    hw1-revised

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,6 +1,10 @@
 // #2 //
 
 function capitalize(str) {
+    if (str[0] >= 'a' && str[0] <= 'z') {
+        str = str.replace(str[0], str[0].toUpperCase());
+    }
+    return str;
 
 }
 
--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -1,4 +1,11 @@
+//#4//
 function calculate(num) {
+    for (var i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            console.log(i)
+        }
+    }
 
 }
 
+calculate(9)
