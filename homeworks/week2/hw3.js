5b785190b1ba0459537f111d91c6f6df248a247d
at7211 <at7211@gmail.com>
2018-09-08 15:27:51 +0800

    hw2-3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,19 @@
 function isPrime(n) {
-  
+    var num = '';
+    for (var i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            num += i;
+        }
+
+    }
+
+    if (num.length <= 2) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
+isPrime(8)
 module.exports = isPrime
