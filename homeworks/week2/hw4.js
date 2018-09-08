86d358dac2a148ae5db7344b2537fd17147418a0
at7211 <at7211@gmail.com>
2018-09-08 17:05:46 +0800

    hw2-4 finished

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,17 @@
 function isPalindromes(str) {
-  
+    for (var i = 0; i <= str.length; i++) {
+        if (str[i] != str[str.length - 1 - i]) {
+            var paslindrome = 1;
+        }
+    }
+
+    if (paslindrome === 1) {
+        console.log("false");
+    } else {
+        console.log("true")
+    }
+
 }
 
+isPalindromes("abcsasdasdsba");
 module.exports = isPalindromes
